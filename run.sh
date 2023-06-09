03ff1f27cd06e82113de7ee4ee1a3aefd6e35265
Bruno Sampaio <bruno.sampaio08@unifesp.br>
2023-06-09 14:48:12 -0300

    change: make gdb work better
    gdb can now break after exec and print things related
    to the new executable that has been loaded into the
    memory space of the fork
    
    debugging fork also works well
    
    TODO: we need a hardcoded breakpoint on shell.c so we can
    change inferiors, check if I can make that runtime

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ TMP_DIR="./tmp"
 
 [ ! -d "$TMP_DIR" ] && mkdir "$TMP_DIR"
 
-./bin/pE_application &
-pid=$(ps -C "pE_application" -o pid=)
-echo "$pid"
-gdb --batch  --command=examine_memory.gdb --pid="$pid" 2>&1
+#./bin/pE_application &
+#pid=$(ps -C "pE_application" -o pid=)
+#echo "$pid"
+gdb --batch  --command=examine_memory.gdb ./bin/pE_application 2>&1
 #pE_jid=$(jobs | grep pE_application | grep -oE "^\[[0-9]\]" | sed 's/\[//g' | sed 's/\]//g')
 #fg
