36198b324a62320de760b971a17aa49f0de95e47
Bruno Sampaio <bruno.sampaio08@unifesp.br>
2023-09-14 21:57:32 -0300

    change: pELOG: add linenumber
    add linenumber to error logs

--- pE_application.h
+++ pE_application.h
@@ -1,6 +1,6 @@
 
 #define pELOG(fmt, ...) \
-	fprintf(stderr, "[%s] [%s]: " fmt "\n", \
-			LOG_TAG, __func__, ##__VA_ARGS__);
+	fprintf(stderr, "[%s] [%s]: %d: " fmt "\n", \
+			LOG_TAG, __func__, __LINE__, ##__VA_ARGS__);
 
 void gdb_run(char *str);
