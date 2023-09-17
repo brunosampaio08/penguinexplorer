e5775d5820ffe42147d80cb7e2052f2108ca00db
Bruno Sampaio <bruno.sampaio08@unifesp.br>
2023-09-17 15:59:18 -0300

    change: pE_application: move declarations to header file
    move function declarations to header file and create de
    header name space to avoid double definition when including

--- pE_application.c
+++ pE_application.c
@@ -27,40 +27,6 @@
 
 void gdb_run(char *str) {}
 
-struct window_desc {
-	int height;
-	int width;
-	int starty;
-	int startx;
-	int pointer_y;
-	int pointer_x;
-	WINDOW *win;
-};
-
-enum tutorial_command_t
-{
-	PSTACK,
-	COMMAND,
-	PCODE,
-	PTUTORIAL,
-	EXIT_CMD,
-}t_cmd_type;
-
-void print_code(FILE* tutorial_file, FILE* code_file);
-
-void print_to_window(char* message, struct window_desc *window);
-
-void read_and_print_file(FILE *file, char* start_regex,\
-		char* stop_regex, struct window_desc *window, int should_print_start, int should_print_stop,\
-		int cflags);
-
-void print_stack(struct window_desc* stack_window, FILE* stack_file);
-
-int exec_tutorial_command(struct window_desc *tutorial_window, FILE* tutorial_file, char* command);
-
-void print_tutorial(int tutorial_number, \
-		struct window_desc *tutorial_window, struct window_desc *stack_window, struct window_desc *command_window);
-
 void start_pE(struct window_desc *tutorial_window)
 {
 	// show some info about pE
--- pE_application.h
+++ pE_application.h
@@ -1,6 +1,46 @@
+#ifndef PE_APPLICATION_H
+#define PE_APPLICATION_H
+
+#include <ncurses.h>
 
 #define pELOG(fmt, ...) \
 	fprintf(stderr, "[%s] [%s]: %d: " fmt "\n", \
 			LOG_TAG, __func__, __LINE__, ##__VA_ARGS__);
 
+struct window_desc {
+	int height;
+	int width;
+	int starty;
+	int startx;
+	int pointer_y;
+	int pointer_x;
+	WINDOW *win;
+};
+
+enum tutorial_command_t
+{
+	PSTACK,
+	COMMAND,
+	PCODE,
+	PTUTORIAL,
+	EXIT_CMD,
+};
+
+void print_code(FILE* tutorial_file, FILE* code_file);
+
+void print_to_window(char* message, struct window_desc *window);
+
+void read_and_print_file(FILE *file, char* start_regex,\
+		char* stop_regex, struct window_desc *window, int should_print_start, int should_print_stop,\
+		int cflags);
+
+void print_stack(struct window_desc* stack_window, FILE* stack_file);
+
+int exec_tutorial_command(struct window_desc *tutorial_window, FILE* tutorial_file, char* command);
+
+void print_tutorial(int tutorial_number, \
+		struct window_desc *tutorial_window, struct window_desc *stack_window, struct window_desc *command_window);
+
 void gdb_run(char *str);
+
+#endif /* PE_APPLICATION_H */
